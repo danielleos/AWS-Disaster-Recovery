eeea555c331b6dae9cab01bdcba5a1acbc497998
Danielle O'Sullivan <danielleos@users.noreply.github.com>
2017-12-12 17:58:37 +0000

    Update merge-params.py

--- merge-params.py
+++ merge-params.py
@@ -1,23 +1,14 @@
 
-# coding: utf-8
-
-# In[10]:
-
-
 import yaml
 from itertools import izip
 
 # Load original environment file
-with open('C:/Users/danielle.osullivan/OneDrive - Accenture/Vodafone/main_prod.yml') as stream:
+with open('main_prod.yml') as stream:
     main_prod = yaml.safe_load(stream)
 
 # Load new ImageIds file
-with open('C:/Users/danielle.osullivan/OneDrive - Accenture/Vodafone/new_params.yml') as stream2:
+with open('new_params.yml') as stream2:
     new_params = yaml.safe_load(stream2)
-
-
-# In[11]:
-
 
 # Function for finding the 'ImageId' in nested dictionary
 def findImageId(d):
@@ -32,23 +23,11 @@ def findImageId(d):
             temp.append(d['RoleParams'][i]['ImageId'])
     return temp
 
-
-# In[12]:
-
-
 # Lists all roles and associated AMI IDs
 originalIds = findImageId(main_prod)
 
-
-# In[13]:
-
-
 # Lists all roles that need updating (which should be all entries)
 newIds = findImageId(new_params)
-
-
-# In[14]:
-
 
 # Convert lists to dictionaries
 i = iter(newIds)
@@ -57,10 +36,6 @@ newIds = dict(izip(i,i))
 j = iter(originalIds)
 # Change name to 'mergedIds' for comprehension
 mergedIds = dict(izip(j,j))
-
-
-# In[15]:
-
 
 # Change the AMI IDs for those that need updating
 # For every key in the mergedIds dictionary
@@ -72,22 +47,15 @@ for i in mergedIds:
             # Then update the ID
             mergedIds[i] = newIds[j]
 
-
-# In[16]:
-
-
 # Update new ImageIds and write to updated file
 import ruamel.yaml as ryaml
 from ruamel.yaml.util import load_yaml_guess_indent as lygi
 
 # Set data and indentation parameters for orignal YAML file
-config, ind, bsi = lygi(open('C:/Users/danielle.osullivan/OneDrive - Accenture/Vodafone/main_prod.yml'))
+config, ind, bsi = lygi(open('main_prod.yml'))
 
 # Variable name for data inside the 'RoleParams' key (i.e. ignoring 'BaseParams')
 RoleParams = config['RoleParams']
-
-
-# In[17]:
 
 
 # Function to update AMI IDs
@@ -104,9 +72,5 @@ def updateImageId(mergedIds):
 updateImageId(mergedIds)
 
 
-# In[18]:
-
-
 # Write to new YAML file
 ryaml.round_trip_dump(config, open('output.yml', 'w'), indent=ind, block_seq_indent=bsi)
-
