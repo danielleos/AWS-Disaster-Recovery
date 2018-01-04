9d612342b0457b31e56743be41f419c8d3a01af9
Danielle O'Sullivan <danielleos@users.noreply.github.com>
2018-01-04 14:17:14 +0000

    Update s3-ss-meta-json.py

--- s3-ss-meta-json.py
+++ s3-ss-meta-json.py
@@ -1,26 +1,67 @@
-import json
+# Import Statements
+import boto3
+import botocore
+import yaml
+import ruamel_yaml as ryaml
+from ruamel_yaml.util import load_yaml_guess_indent as lygi
 from itertools import izip
+from datetime import datetime
+from dateutil import parser
+from dateutil.parser import parse
 
-# Load local file
-with open('main_prod.yml') as stream:
-    main_prod = yaml.safe_load(stream)
+# Read the AMIs that exist using describe-images(**kwargs)
+# Filter using specific tag-values
+try:
+    print "Trying to read the AMIs..."
+    images = ec2.describe_images(
+       Filters=[
+           {
+                'Name': 'owner-id',
+                'Values': ['00000000000'],
+                'Name': 'tag-key',
+                'Values': ['Name'],
+                'Name': 'tag-value',
+                'Values': ['name-of-the-ec2-instance']
+            },
+        ],
+    )
+    print "Success"
+except botocore.exceptions.ClientError as e:
+    # If a client error is thrown, then check that it was a 404 error.
+    # If it was a 404 error, then the bucket does not exist.
+    error_code = int(e.response['Error']['Code'])
+    print e.response['Error']
+    success = False
+    if error_code == 404:
+        exists = False
+        print e.response['Error']
+#print images
 
-# Function for finding the 'ImageId' in nested dictionary
-def findImageId(d):
-    # Create empty list
-    temp = []
-    # For every role
-    for i in d['RoleParams']:
-        # Find the 'ImageId' property
-        if 'ImageId' in d['RoleParams'][i]:
-            # Output both the role name and the value of the 'ImageId' associated with it
-            temp.append(i)
-            temp.append(d['RoleParams'][i]['ImageId'])
-    return temp
+# Find the AMIs (IDs) with most recent creation date
+# Create a temporary lists
+names = []
+image_ids = []
+creation_dates = []
+# For every entry in the list images
+for image in range(len(images['Images'])):
+    names.append(images['Images'][image]['Name'])
+    image_ids.append(images['Images'][image]['ImageId'])
+    creation_dates.append(images['Images'][image]['CreationDate'])
+    print names, image_ids, creation_dates
 	
-# Lists all roles and associated AMI IDs
-originalIds = findImageId(main_prod)
+# Transform date strings into datetime objects
+# NOT FINALISED
+parser.parserinfo(yearfirst=True, dayfirst=False)
+now = datetime.now()
+#print now
+dates = []
+for name in names:
+    for i in range(len(creation_dates)):
+        dates.append(parse(creation_dates[i], fuzzy=True))
+        #print dates[i]
+    
+recent = max(dates)
+index = dates.index(recent)
 
-j = iter(originalIds)
-# Change name to 'mergedIds' for comprehension
-originalIds = dict(izip(j,j))
+# Put those AMI IDs into main_prod.yml file
+# Code omitted
